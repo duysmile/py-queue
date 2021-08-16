0d2b1f1573f0295e0dac06661555562e4135b7bd
duy.nguyen <duy210697@gmail.com>
2021-08-16 15:36:43 +0700

    [dev] simple queue with rabbit

---
+++ simple-queue/provider.js
@@ -0,0 +1,30 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (err, connection) => {
+    if (err) {
+        throw err;
+    }
+
+    connection.createChannel((err, channel) => {
+        if (err) {
+            throw err;
+        }
+
+        const queue = 'hello';
+        const message = process.argv.slice(2).join(' ') || "Hello World!";
+
+        channel.assertQueue(queue, {
+            durable: false,
+        });
+
+        channel.sendToQueue(queue, Buffer.from(message), {
+            persistent: true,
+        });
+        console.log(' [x] Send %s', message);
+
+        setTimeout(function () {
+            connection.close();
+            process.exit(0)
+        }, 500);
+    });
+});
