7998a09c3ed1a5c8126cb161433ec340e4667694
duy.nguyen <duy210697@gmail.com>
2021-08-16 16:11:41 +0700

    [dev] ack queue with rabbit

--- simple-queue/consumer.js
+++ simple-queue/consumer.js
@@ -18,9 +18,15 @@ amqp.connect('amqp://localhost', (err, connection) => {
 
         console.log(' [x] Waiting for message in %s. To exit press Ctrl + C', queue);
         channel.consume(queue, msg => {
+            const secs = msg.content.toString().split('.').length - 1;
             console.log(' [x] Receive %s', msg.content.toString());
+
+            setTimeout(function () {
+                console.log(" [x] Done");
+                channel.ack(msg);
+            }, secs * 1000);
         }, {
-            noAck: true,
+            noAck: false,
         });
     });
 })
